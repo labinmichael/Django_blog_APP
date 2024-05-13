580118e3fa3892471c26000680cd25dd033e6ca0
labinmichael <abinvalliyil@gmail.com>
2024-05-13 08:19:44 +0530

    blog update api patch method completed

--- home/views.py
+++ home/views.py
@@ -44,8 +44,28 @@ class BlogView(APIView):
                         
             return Response({"data":{},
                             "message":"something went wrong"
-                                },status=status.HTTP_400_BAD_REQUEST)               
+                                },status=status.HTTP_400_BAD_REQUEST)   
 
+    def patch(self, request):
+        try:
+            data = request.data
+            blog = Blog.objects.filter(uid=data.get('uid')).first()  # Select the first object from the queryset
+            
+            if not blog:
+                return Response({"data": {}, "message": "Invalid blog UID"}, status=status.HTTP_400_BAD_REQUEST)
+        
+            if not request.user == blog.user: 
+                return Response({"data": {}, "message": "You are not authorized to do this"}, status=status.HTTP_400_BAD_REQUEST)        
+
+            serializer = BlogSerializer(blog, data=data, partial=True)   
+            if not serializer.is_valid():
+                return Response({"data": serializer.errors, "message": "Something went wrong"}, status=status.HTTP_400_BAD_REQUEST)
+            
+            serializer.save()
+            return Response({"data": serializer.data, "message": "Blog updated successfully"}, status=status.HTTP_200_OK)
+        
+        except Exception as e:
+            return Response({"data": {}, "message": f"Something went wrong"}, status=status.HTTP_400_BAD_REQUEST)
 
   
 class BlogSearchView(APIView):
